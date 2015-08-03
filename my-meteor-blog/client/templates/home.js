025a0a2567299aaa107dbab2e3eefc587a14273f
Ryan Hammang <ryan.hammang@gmail.com>
2015-08-02 19:09:21 -0600

    done with mac. still on ch2

---
+++ my-meteor-blog/client/templates/home.js
@@ -0,0 +1,25 @@
+Template.home.created = function () {
+  console.log('Created the home template');
+};
+
+Template.home.rendered = function () {
+  console.log('Rendered the home template');
+};
+
+Template.home.destroyed = function () {
+  console.log('Destroyed the home template');
+};
+
+Template.home.helpers({
+  exampleHelper: function () {
+    return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+  }
+  dataContextHelper: function () {
+    return {
+      someText: 'This text was set using a helper of a parent template.',
+      someNested: {
+        text: 'That comes from "someNested.text"'
+      }
+    };
+  }
+});
